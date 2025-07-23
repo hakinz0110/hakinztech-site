6251a0fe73a0ee66a531341efb261930f6fa27b9
Abere Akinola <AbereAkinola@gmail.com>
2025-07-23 00:27:26 +0000

    this is the url to the image i want there{https://luonahsbhiopdibgxutp.s

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -60,7 +60,7 @@ export function About() {
             <div className="order-1 md:order-2">
                 <Card className="overflow-hidden shadow-lg border-2 border-primary/20 transition-transform duration-300 hover:scale-105 hover:shadow-primary/20 p-4">
                 <Image
-                    src="https://placehold.co/600x700"
+                    src="https://luonahsbhiopdibgxutp.supabase.co/storage/v1/object/public/portfolio-images//Akinola%20Abere.png"
                     alt="Hakinz_Tech"
                     width={600}
                     height={700}
